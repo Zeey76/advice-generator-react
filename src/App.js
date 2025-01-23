631fc4f40b542a0442b559ab692d6b28b9cc6001
Zeey76 <abdulrasaqzainab76@gmail.com>
2025-01-23 22:22:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,47 +4,55 @@ import { useState, useEffect } from "react";
 
 const App = () => {
   const [advice, setAdvice] = useState({});
-  const [isLoading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  
   async function fetchAdvice() {
     setLoading(true);
     setError(null);
-    
+
     try {
-      
       await new Promise((resolve) => setTimeout(resolve, 300));
       const response = await fetch("https://api.adviceslip.com/advice");
-      if (!response.ok) throw new Error('Failed to fetch advice');
+      if (!response.ok) throw new Error("Failed to fetch advice");
       const data = await response.json();
       setAdvice(data.slip);
-      
-    } catch(err) {
-      setError(err)
-      
+    } catch (err) {
+      setError(err);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
   useEffect(() => {
-    fetchAdvice()
-  }, [])
+    fetchAdvice();
+  }, []);
 
   return (
     <div class="advice-generator">
-      {!isLoading && !error && <p className="heading">Advice #<span className="id-number">{advice.id}</span></p>}
+      {!isLoading && !error && (
+        <p className="heading">
+          Advice #<span className="id-number">{advice.id}</span>
+        </p>
+      )}
       {!isLoading && !error && <p className="quote">{advice.advice}</p>}
       {isLoading && <p className="quote">Loading...</p>}
       {error && <p className="quote">Oops! Something went wrong. Try again!</p>}
-      <img src="./images/pattern-divider-desktop.svg" className="desktop-image divider" alt="" />
-      <img src="./images/pattern-divider-mobile.svg" className="mobile-image divider" alt="" />
+      <img
+        src="./images/pattern-divider-desktop.svg"
+        className="desktop-image divider"
+        alt=""
+      />
+      <img
+        src="./images/pattern-divider-mobile.svg"
+        className="mobile-image divider"
+        alt=""
+      />
       <button className="dice" onClick={fetchAdvice}>
-        <img src="./images/icon-dice.svg" alt=""/>
+        <img src="./images/icon-dice.svg" alt="" />
       </button>
     </div>
-  )
-}
+  );
+};
 
 export default App;
