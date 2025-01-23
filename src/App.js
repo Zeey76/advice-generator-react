952170bf617c593fd967049eac563daa9a9fb16a
Zeey76 <abdulrasaqzainab76@gmail.com>
2025-01-23 22:21:22 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
   const [advice, setAdvice] = useState({});
   const [isLoading, setLoading] = useState(false)
   const [error, setError] = useState(null)
